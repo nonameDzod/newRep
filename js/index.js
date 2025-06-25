ee51e19eb3cdfbe0a35ab6a6891df6749afa017a
marat dzodziev <minamota80@gmail.com>
2025-06-25 17:29:57 +0300

    create calcul interface

--- js/index.js
+++ js/index.js
@@ -1,21 +1,12 @@
-// 
-// n1 = document.querySelector('.first-value').value
-// n1 = parseInt(n1)
-// n2 = document.querySelector('.second-value').value
-// n2 = parseInt(n2)
-// let rez
-// let out = document.querySelector('.rezult')
+let num1,num2,rezult
 
-// console.log(out)
+num1 = parseInt(num1 = document.getElementById('num1').value)
 
 function plus() 
 {
-    let num1,num2,rezult
-    num1 = document.getElementById('num1').value
-    num1 = parseInt(num1)
-
+    
     num2 = document.getElementById('num2').value
-    num2 = parseInt(num2)
+    num2 = parseInt()
     rezult = num1 + num2 
     document.getElementById('out').innerHTML = rezult
     console.log(rezult)
